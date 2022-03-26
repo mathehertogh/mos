c2abab0bd8a47c4994e9889ff2f1f17f3083e414
mathe <m.c.hertogh@gmail.com>
2022-03-26 12:06:38 +0100

    Call global constructors

--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -1,16 +1,26 @@
 #include <types.h>
+#include <segmentation.h>
 
 int y = 5;
 
+typedef void(*global_constructor)(void);
+extern "C" global_constructor ctors;
+
 int kernel_main(uint16_t boot_drive_number, char *mmap) __attribute__((section (".entry")));
 
-int kernel_main(uint16_t boot_drive_number, char *mmap) {
-	int x;
+void call_global_constructors()
+{
+	global_constructor *ctor = &ctors;
+	while (*ctor) {
+		(*ctor)();
+		++ctor;
+	}
+}
 
-	x = 7;
-
-	x += (int)boot_drive_number;
-	x *= y;
+int kernel_main(uint16_t boot_drive_number, char *mmap)
+{
+	// TODO initialize .bss section with zeros?
+	call_global_constructors();
 
 	while(true);
 
